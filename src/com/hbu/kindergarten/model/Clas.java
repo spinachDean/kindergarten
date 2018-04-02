d474586ea6408f23b007d127cc11577379b15dfe
kevinlyz <hbukevin@gmail.com>
2018-04-02 11:24:21 +0800

    提交班级Dao

--- src/com/hbu/kindergarten/model/Clas.java
+++ src/com/hbu/kindergarten/model/Clas.java
@@ -8,44 +8,121 @@ public class Clas {
 	private String cName;			//班级名称
 	private String cAlias;			//班级别名
 	private Integer cContent;		//班级容量
-	private String cConcact;		//联系方式
+	private String cTeacher;		//联系方式
 	private String cDescription;     //班级描述
 	private String username;       //用户名
-	private String kgId;          //幼儿园id
+	private Integer kgId;
+	private String kgName;          //幼儿园名称
 	private Date INSDATE;        //插入日期
 	private Integer INSUSERID;			//插入用户id
-	private Date UPDATET;			//更新日期
+	private Date UPDATE;			//更新日期
 	private Integer UPUSERID;			//更新用户id
-
-	
-	public Clas(Integer cId, String cName, String cAlias, Integer cContent, String cConcact, String cDescription,
-			String username, String kgId, Date iNSDATE, Integer iNSUSERID, Date uPDATET, Integer uPUSERID) {
+	public Integer getcId() {
+		return cId;
+	}
+	public void setcId(Integer cId) {
+		this.cId = cId;
+	}
+	public String getcName() {
+		return cName;
+	}
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+	public String getcAlias() {
+		return cAlias;
+	}
+	public void setcAlias(String cAlias) {
+		this.cAlias = cAlias;
+	}
+	public Integer getcContent() {
+		return cContent;
+	}
+	public void setcContent(Integer cContent) {
+		this.cContent = cContent;
+	}
+	public String getcTeacher() {
+		return cTeacher;
+	}
+	public void setcTeacher(String cTeacher) {
+		this.cTeacher = cTeacher;
+	}
+	public String getcDescription() {
+		return cDescription;
+	}
+	public void setcDescription(String cDescription) {
+		this.cDescription = cDescription;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Integer getKgId() {
+		return kgId;
+	}
+	public void setKgId(Integer kgId) {
+		this.kgId = kgId;
+	}
+	public String getKgName() {
+		return kgName;
+	}
+	public void setKgName(String kgName) {
+		this.kgName = kgName;
+	}
+	public Date getINSDATE() {
+		return INSDATE;
+	}
+	public void setINSDATE(Date iNSDATE) {
+		INSDATE = iNSDATE;
+	}
+	public Integer getINSUSERID() {
+		return INSUSERID;
+	}
+	public void setINSUSERID(Integer iNSUSERID) {
+		INSUSERID = iNSUSERID;
+	}
+	public Date getUPDATE() {
+		return UPDATE;
+	}
+	public void setUPDATE(Date uPDATE) {
+		UPDATE = uPDATE;
+	}
+	public Integer getUPUSERID() {
+		return UPUSERID;
+	}
+	public void setUPUSERID(Integer uPUSERID) {
+		UPUSERID = uPUSERID;
+	}
+	public Clas(Integer cId, String cName, String cAlias, Integer cContent, String cTeacher, String cDescription,
+			String username, Integer kgId, String kgName, Date iNSDATE, Integer iNSUSERID, Date uPDATE,
+			Integer uPUSERID) {
 		super();
 		this.cId = cId;
 		this.cName = cName;
 		this.cAlias = cAlias;
 		this.cContent = cContent;
-		this.cConcact = cConcact;
+		this.cTeacher = cTeacher;
 		this.cDescription = cDescription;
 		this.username = username;
 		this.kgId = kgId;
+		this.kgName = kgName;
 		INSDATE = iNSDATE;
 		INSUSERID = iNSUSERID;
-		UPDATET = uPDATET;
+		UPDATE = uPDATE;
 		UPUSERID = uPUSERID;
 	}
-
-
 	public Clas() {
 		super();
 	}
-
-
 	@Override
 	public String toString() {
-		return "Class [cId=" + cId + ", cName=" + cName + ", cAlias=" + cAlias + ", cContent=" + cContent
-				+ ", cConcact=" + cConcact + ", cDescription=" + cDescription + ", username=" + username + ", kgId="
-				+ kgId + ", INSDATE=" + INSDATE + ", INSUSERID=" + INSUSERID + ", UPDATET=" + UPDATET + ", UPUSERID="
-				+ UPUSERID + "]";
+		return "Clas [cId=" + cId + ", cName=" + cName + ", cAlias=" + cAlias + ", cContent=" + cContent + ", cTeacher="
+				+ cTeacher + ", cDescription=" + cDescription + ", username=" + username + ", kgId=" + kgId
+				+ ", kgName=" + kgName + ", INSDATE=" + INSDATE + ", INSUSERID=" + INSUSERID + ", UPDATE=" + UPDATE
+				+ ", UPUSERID=" + UPUSERID + "]";
 	}
+
+
 }
